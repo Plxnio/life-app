83155b59c3d2b3a14bdc05b8d1acfcea3c6a2466
Plxnio <junior.pmr@gmail.com>
2026-02-21 12:22:17 -0300

    fix: 5

--- static/script.js
+++ static/script.js
@@ -95,7 +95,10 @@ async function loginGoogle() {
     const { data, error } = await supabaseClient.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: window.location.origin 
+            queryParams: {
+                access_type: 'offline',
+                prompt: 'consent',
+            }
         }
     });
 }
